61c5fd57ee195fbaebbfa8caeb7ec9948952b85a
w2gd <W2GDong@gmail.com>
2022-07-04 22:39:35 +0800

    stateless组件学习3

--- flutter_demo/lib/common/routes.dart
+++ flutter_demo/lib/common/routes.dart
@@ -5,7 +5,10 @@ import 'package:flutter_demo/sample/timeline_page.dart';
 import 'package:flutter_demo/widget/stateful/image_widget.dart';
 import 'package:flutter_demo/widget/stateful/pageviewcontrol.dart';
 import 'package:flutter_demo/widget/stateful/sliverappbar_widget.dart';
+import 'package:flutter_demo/widget/stateless/aboutlisttile_widget.dart';
 import 'package:flutter_demo/widget/stateless/actionchip_widget.dart';
+import 'package:flutter_demo/widget/stateless/button_widget.dart';
+import 'package:flutter_demo/widget/stateless/card_widget.dart';
 import 'package:flutter_demo/widget/stateless/chip_widget.dart';
 import 'package:flutter_demo/widget/stateless/choicechip_widget.dart';
 import 'package:flutter_demo/widget/stateless/circleavatar_widget.dart';
@@ -20,6 +23,14 @@ import 'package:flutter_demo/widget/stateless/singlechildscrollview_widget.dart'
 import 'package:flutter_demo/widget/stateless/text_widget.dart';
 import 'package:flutter_demo/widget/stateless/theme_widget.dart';
 import 'package:flutter_demo/widget/stateless/useraccountdrawerheader_widget.dart';
+import 'package:flutter_demo/widget/stateless/visiblity_widget.dart';
+import '../widget/stateless/alertdialog_widget.dart';
+import '../widget/stateless/checkboxlisttile_widget.dart';
+import '../widget/stateless/gridtile_widget.dart';
+import '../widget/stateless/listtitle_widget.dart';
+import '../widget/stateless/radiolisttile_widget.dart';
+import '../widget/stateless/spacer_widget.dart';
+import '../widget/stateless/switchlisttile_widget.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   '/index': (context) => const Index(),
@@ -41,10 +52,21 @@ Map<String, Widget Function(BuildContext)> routes = {
   '/gesturedetector': (context) => const GestureDetectorWidget(),
   '/useraccountdrawerheader': (context) =>
       const UserAccountDrawerHeaderWidget(),
+  '/button': (context) => const ButtonWidget(),
+  '/card': (context) => const CardWidget(),
+  '/visiblity': (context) => const VisiblityWidget(),
   // 有状态组件导航
   '/image': (context) => const ImageWidget(),
   '/sliverappbar': (context) => const SliverAppBarWidget(),
   // 示例导航
   '/plant-shop': (context) => const PlantShop(),
   '/timeline': (context) => const TimelinePage(),
+  '/listtile': (context) => const ListTileWidget(),
+  '/checkboxlisttile': (context) => const CheckboxListTileWidget(),
+  '/switchlisttile': (context) => const SwitchListTileWidget(),
+  '/radiolisttile': (context) => const RadioListTileWidget(),
+  '/gridtile': (context) => const GridTileWidget(),
+  '/aboutlisttile': (context) => const AboutListTileWidget(),
+  "/spacer": (context) => const SpacerWidget(),
+  "/alertdialog": (context) => const AlertDialogWidget(),
 };
---
+++ flutter_demo/lib/widget/stateless/button_widget.dart
@@ -0,0 +1,293 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/common/style.dart';
+
+class ButtonWidget extends StatelessWidget {
+  const ButtonWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var buttons = {
+      Colors.red: Icons.add,
+      Colors.blue: Icons.bluetooth,
+      Colors.green: Icons.android,
+    };
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Button组件'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                'MaterialButton材质按钮',
+                style: titleStyle,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                ),
+                child: const Text(
+                  '基于RawMaterialButton实现的通用Material按钮。可盛放一个子组件，能定义颜色、形状等表现，可接收点击和长按事件。',
+                  style: descStyle,
+                ),
+              ),
+              MaterialButton(
+                height: 40,
+                elevation: 5,
+                color: Colors.blue,
+                highlightColor: Colors.green,
+                splashColor: Colors.orange,
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(8),
+                child: const Text('MaterialButton'),
+                onLongPress: () => showAboutDialog(context: context),
+                onPressed: () => print('onPressed'),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 5.0,
+                ),
+                child: const Text(
+                  '自定义MaterialButton',
+                  style: titleStyle,
+                ),
+              ),
+              SizedBox(
+                width: 40,
+                height: 40,
+                child: MaterialButton(
+                  padding: const EdgeInsets.all(0),
+                  textColor: const Color(0xFFEFFFFF),
+                  elevation: 5,
+                  color: Colors.blue,
+                  highlightColor: const Color(0xFFF88B0A),
+                  splashColor: Colors.red,
+                  shape: const CircleBorder(
+                    side: BorderSide(
+                      width: 2.0,
+                      color: Color(0xFFFDFDFD),
+                    ),
+                  ),
+                  onLongPress: () => showAboutDialog(context: context),
+                  onPressed: () => print('onPressed'),
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                ),
+                width: 100,
+                height: 40,
+                child: MaterialButton(
+                  padding: const EdgeInsets.all(0),
+                  textColor: const Color(0xFFEFFFFF),
+                  elevation: 5,
+                  color: Colors.blue,
+                  highlightColor: const Color(0xFFF88B0A),
+                  splashColor: Colors.red,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  onLongPress: () => print('onLongPress'),
+                  onPressed: () => print('onPressed'),
+                  child: const Icon(
+                    Icons.remove,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const Text(
+                'FloatingActionButton浮动按钮',
+                style: titleStyle,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                ),
+                child: const Text(
+                  '浮动按钮，一般用于Scaffold中，可摆放在特定位置。可盛放一个子组件，接收点击事件，可定义颜色、形状等。',
+                  style: descStyle,
+                ),
+              ),
+              Wrap(
+                spacing: 20,
+                children: buttons.keys
+                    .map(
+                      (e) => FloatingActionButton(
+                        onPressed: () {},
+                        heroTag: '$e',
+                        backgroundColor: e,
+                        mini: true,
+                        foregroundColor: Colors.white,
+                        tooltip: 'FAB',
+                        elevation: 5,
+                        child: Icon(buttons[e]),
+                      ),
+                    )
+                    .toList(),
+              ),
+              const Text(
+                'TextButton文本按钮',
+                style: titleStyle,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                ),
+                child: const Text(
+                  '无阴影无水波纹的文本按钮，基于MaterialButton实现，所有属性和MaterialButton类似。',
+                  style: descStyle,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  onLongPress: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      Colors.deepPurple,
+                    ),
+                  ),
+                  child: const Text(
+                    'TextButton',
+                    style: titleLightStyle,
+                  ),
+                ),
+              ),
+              const Text(
+                'OutlinedButton边框按钮',
+                style: titleStyle,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                ),
+                child: const Text(
+                  '边框样式按钮，基于MaterialButton实现，所有属性和MaterialButton类似。',
+                  style: descStyle,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                ),
+                child: OutlinedButton(
+                  onPressed: () {},
+                  onLongPress: () {},
+                  child: const Text(
+                    'OutlinedButton',
+                    style: titleStyle,
+                  ),
+                ),
+              ),
+              const Text(
+                'ElevatedButton浮起按钮',
+                style: titleStyle,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                ),
+                child: const Text(
+                  '有阴影的浮起按钮，基于MaterialButton实现，所有属性和MaterialButton类似。',
+                  style: descStyle,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                ),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  onLongPress: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      Colors.blue,
+                    ),
+                    shadowColor: MaterialStateProperty.all(
+                      Colors.orange,
+                    ),
+                  ),
+                  child: const Text(
+                    'ElevatedButton',
+                    style: titleLightStyle,
+                  ),
+                ),
+              ),
+              const Text(
+                'ButtonBar按钮栏',
+                style: titleStyle,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                ),
+                child: const Text(
+                  '接收按钮组件列表，常用于盛放若干个按钮，可指定对齐方式、边距等信息。',
+                  style: descStyle,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                child: ButtonBar(
+                  alignment: MainAxisAlignment.spaceBetween,
+                  buttonHeight: 40,
+                  buttonPadding: const EdgeInsets.symmetric(horizontal: 15),
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Colors.blue,
+                        ),
+                        shadowColor: MaterialStateProperty.all(
+                          Colors.orange,
+                        ),
+                      ),
+                      child: const Text('Elevated'),
+                    ),
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Outlined',
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Colors.blue,
+                        ),
+                      ),
+                      child: const Text(
+                        'Text',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
